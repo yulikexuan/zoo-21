221f605f77e493dd9818b5a54ad471b90693f42d
yulikexuan <yulikexuan@gmail.com>
2025-10-31 10:38:55 -0400

    OCP-21 Open zoo.animal.talks.content to zoo.staff

--- staff/zoo/staff/Jobs.java
+++ staff/zoo/staff/Jobs.java
@@ -2,4 +2,11 @@
 
 package zoo.staff;
 
-public class Jobs {}
+import zoo.animal.talks.content.*;
+
+public class Jobs {
+    public static void main(String... args) {
+        System.out.println(new ElephantScript().script());
+	System.out.println(new SeaLionScript().script());
+    }
+}
